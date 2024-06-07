a09a81bbd80f3edb94f7f4f8727dea4774495466
montaserhatamleh <montaser.alhatamleh>
2024-06-07 03:40:55 +0300

    finish setup

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,27 @@ const cors = require("cors");
 require("dotenv").config();
 require("./models/db");
 
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const applyRouter = require("./routes/apply");
+const categoryRouter = require("./routes/category");
+const createJobRouter = require("./routes/createJob");
+const roleRouter = require("./routes/role");
+const userRouter = require("./routes/user");
 
 app.use(cors());
 app.use(express.json());
 
 // Handles any other endpoints [unassigned - endpoints]
+
+//Routes middleware
+app.use("/apply", applyRouter);
+app.use("/category", categoryRouter);
+app.use("/createJob", createJobRouter);
+app.use("/role", roleRouter);
+app.use("/user", userRouter);
+
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT, () => {
